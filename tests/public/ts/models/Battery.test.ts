37b5260d3970a0f315f894ae02e8b78e609c9383
Schmaj <jwschmalz@outlook.com>
2019-03-26 17:15:06 -0400

    Added CurrentSource & test

--- tests/public/ts/models/Battery.test.ts
+++ tests/public/ts/models/Battery.test.ts
@@ -5,8 +5,8 @@ import {Battery} from "../../../../site/public/ts/models/eeobjects/Battery";
 describe("Battery", () => {
     describe("New Battery, Default", () => {
         const battery = new Battery();
-        //Default to 1 Volt
-        expect(battery.getVoltage()).toBe(1);
+        //Default to 5 Volt
+        expect(battery.getVoltage()).toBe(5);
     });
     describe("Example Batteries", () => {
         it ("Positive Voltage", () => {
@@ -14,14 +14,14 @@ describe("Battery", () => {
             expect(battery.getVoltage()).toBe(2);
         });
         it ("Negative Voltage", () => {
-            //Should default to 1 Volt
+            //Should default to 5 Volt
             const battery = new Battery(-1);
-            expect(battery.getVoltage()).toBe(1);
+            expect(battery.getVoltage()).toBe(5);
         });
         it ("Zero Voltage", () => {
-            //Should default to 1 Volt
+            //Should default to 5 Volt
             const battery = new Battery(0);
-            expect(battery.getVoltage()).toBe(1);
+            expect(battery.getVoltage()).toBe(5);
         });
     });
 });
